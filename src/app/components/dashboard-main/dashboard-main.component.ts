1494742327b67c79573119c679e4c773357c6d9a
jovyzabel <jovymonka7@gmail.com>
2024-12-13 19:16:31 +0100

    #1nombre de cours et detudiant,

--- src/app/components/dashboard-main/dashboard-main.component.ts
+++ src/app/components/dashboard-main/dashboard-main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/data.service';
+import {COURSES} from "../../mock-cours";
 @Component({
   selector: 'app-dashboard-main',
   templateUrl: './dashboard-main.component.html',
@@ -7,9 +8,12 @@ import { DataService } from 'src/app/data.service';
 })
 export class DashboardMainComponent implements OnInit {
   totalStudents: number = 0;
+  totalCourses: number = 0;
+  courses  = COURSES
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
     this.totalStudents = this.dataService.getStudents().length;
+    this.totalCourses = this.courses.length;
   }
 }
--- src/app/mock-cours.ts
+++ src/app/mock-cours.ts
@@ -1,9 +1,18 @@
 import { Cours } from './cours';
 
 export const COURSES: Cours[] = [
-  { id: 'I1AR01', theme: 'Réseaux et Télécom', nombreHeures: '20h15min' },
-  { id: 'I1AR02', theme: 'Analyse 1', nombreHeures: '12h30min' },
-  { id: 'I1AR03', theme: 'Algèbre 1', nombreHeures: '15h45min' },
-  { id: 'I1AR04', theme: 'Introduction à l’informatique', nombreHeures: '8h' },
+  { id: 'I1AR01', theme: 'Réseaux et Télécom', nombreHeures: '20h00min' },
+  { id: 'I1AR02', theme: 'Analyse 1', nombreHeures: '20h00min' },
+  { id: 'I1AR03', theme: 'Algèbre 1', nombreHeures: '20h0min' },
+  { id: 'I1AR04', theme: 'Introduction à l’informatique', nombreHeures: '40h' },
   { id: 'I1AR05', theme: 'Algorithmique 1', nombreHeures: '25h30min' },
+  { id: 'I1AR06', theme: 'Modelisation UML', nombreHeures: '20h15min' },
+  { id: 'I1AR07', theme: 'Typescript 5', nombreHeures: '15h00min' },
+  { id: 'I1AR08', theme: 'POO', nombreHeures: '45h00min' },
+  { id: 'I1AR09', theme: 'Supervision et Virtualisation', nombreHeures: '15h' },
+  { id: 'I1AR10', theme: 'Système d\'exploitation', nombreHeures: '30h00min' },
+  { id: 'I1AR11', theme: 'Techniques d\'expression', nombreHeures: '15h00min' },
+  { id: 'I1AR12', theme: 'Algorithmique 2', nombreHeures: '50h00min' },
+  { id: 'I1AR13', theme: 'Electronique', nombreHeures: '20h00min' },
+
 ];
