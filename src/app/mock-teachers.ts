37014affa641c76d579d2a4cd589db8ceeaed748
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-12-13 21:15:08 +0100

    add teachers interface and fonctionalities

---
+++ src/app/mock-teachers.ts
@@ -0,0 +1,31 @@
+import { Teacher } from './interfaces/teachers-interface';
+
+export const TEACHERS: Teacher[] = [
+  {
+    id: 1,
+    nom: 'Dupont',
+    prenom: 'Jean',
+    email: '6GJtD@example.com',
+    telephone: '0612345678',
+    adresse: '12 rue de la paix',
+    dateDAjout: new Date(2010, 8, 5),
+  },
+  {
+    id: 2,
+    nom: 'Durand',
+    prenom: 'Pierre',
+    email: '6GJtD@example.com',
+    telephone: '0612345678',
+    adresse: '12 rue de la paix',
+    dateDAjout: new Date(2010, 8, 5),
+  },
+  {
+    id: 3,
+    nom: 'Martin',
+    prenom: 'Paul',
+    email: '6GJtD@example.com',
+    telephone: '0612345678',
+    adresse: '12 rue de la paix',
+    dateDAjout: new Date(2010, 8, 5),
+  },
+];
