43e35fb473e8771b3e39ad978162f5744e10da61
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-12-14 02:15:06 +0100

    update courses page and fonctionnalities

--- src/app/mock-cours.ts
+++ src/app/mock-cours.ts
@@ -1,9 +1,35 @@
 import { Cours } from './cours';
 
 export const COURSES: Cours[] = [
-  { id: 'I1AR01', theme: 'Réseaux et Télécom', nombreHeures: '20h15min' },
-  { id: 'I1AR02', theme: 'Analyse 1', nombreHeures: '12h30min' },
-  { id: 'I1AR03', theme: 'Algèbre 1', nombreHeures: '15h45min' },
-  { id: 'I1AR04', theme: 'Introduction à l’informatique', nombreHeures: '8h' },
-  { id: 'I1AR05', theme: 'Algorithmique 1', nombreHeures: '25h30min' },
+  {
+    id: 1,
+    title: 'Réseaux et Télécom',
+    duration: '20h15min',
+    description: 'Introduction aux concepts de réseaux et télécommunications.',
+  },
+  {
+    id: 2,
+    title: 'Analyse 1',
+    duration: '12h30min',
+    description: 'Étude des fonctions et des limites en mathématiques.',
+  },
+  {
+    id: 3,
+    title: 'Algèbre 1',
+    duration: '15h45min',
+    description: "Fondements de l'algèbre linéaire et des équations.",
+  },
+  {
+    id: 4,
+    title: "Introduction à l'informatique",
+    duration: '8h',
+    description:
+      "Présentation des bases de l'informatique et de la programmation.",
+  },
+  {
+    id: 5,
+    title: 'Algorithmique 1',
+    duration: '25h30min',
+    description: 'Concepts de base des algorithmes et structures de données.',
+  },
 ];
