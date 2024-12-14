34201124194b9c65f0578f231b4698d0fa4da27b
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-12-14 03:55:47 +0100

    update dashboard charts

--- src/app/components/dashboard-main/dashboard-main.component.ts
+++ src/app/components/dashboard-main/dashboard-main.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CoursesService } from 'src/app/services/courses/courses.service';
 import { StudentsService } from 'src/app/services/students/students.service';
-import { Teacher } from '../../interfaces/teachers-interface';
-import { COURSES } from '../../mock-cours';
-import { TEACHERS } from '../../mock-teachers';
+import { TeachersService } from 'src/app/services/teachers/teachers.service';
 
 @Component({
   selector: 'app-dashboard-main',
@@ -13,25 +12,28 @@ export class DashboardMainComponent implements OnInit {
   totalStudents: number = 0;
   totalCourses: number = 0;
   totalTeachers: number = 0;
-  teachers: Teacher[] = TEACHERS;
-  courses = COURSES;
-  constructor(private studentService: StudentsService) {}
+  constructor(
+    private studentService: StudentsService,
+    private teacherService: TeachersService,
+    private coursesService: CoursesService
+  ) {}
 
   ngOnInit() {
     this.totalStudents = this.studentService.getStudents().length;
-    this.totalCourses = this.courses.length;
-    this.totalTeachers = this.teachers.length;
+    this.totalCourses = this.coursesService.getCourses().length;
+    this.totalTeachers = this.teacherService.getTeachers().length;
   }
+
   // Données pour le graphique
   public barChartData = {
-    labels: ['Etudiants', 'Enseignants', 'Cours'], // Noms des catégories
+    labels: ['Enseignants', 'Etudiants', 'Cours'], // Noms des catégories
     datasets: [
       {
         label: 'Effectif',
         data: [
+          this.teacherService.getTeachers().length,
           this.studentService.getStudents().length,
-          this.teachers.length,
-          this.courses.length,
+          this.coursesService.getCourses().length,
         ], // Données correspondantes
         backgroundColor: ['#006699', '#f1bb35', '#38a3a5'], // Couleurs des barres
         borderColor: ['#f3f4f6', '#f3f4f6', '#f3f4f6'], // Couleurs des bordures
